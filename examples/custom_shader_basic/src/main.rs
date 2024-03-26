43986aba07de33b1efc0d29d18fad5b9f58ca9b3
Ivan Avdeev <me@provod.works>
2024-03-25 20:32:33 -0400

    add basic quad custom shader widget example
    draws mandelbrot with poor aspect

---
+++ examples/custom_shader_basic/src/main.rs
@@ -0,0 +1,195 @@
+use iced::mouse;
+use iced::time::Instant;
+use iced::widget::shader;
+use iced::widget::shader::wgpu;
+use iced::widget::shader::wgpu::Color;
+use iced::window;
+use iced::{Element, Length, Rectangle, Size, Subscription};
+
+fn main() -> iced::Result {
+    iced::program(
+        "Custom Shader - Iced",
+        BasicShader::update,
+        BasicShader::view,
+    )
+    .subscription(BasicShader::subscription)
+    .run()
+}
+
+struct CustomShaderPipeline {
+    pipeline: wgpu::RenderPipeline,
+}
+
+impl CustomShaderPipeline {
+    fn new(device: &wgpu::Device, format: wgpu::TextureFormat) -> Self {
+        let shader =
+            device.create_shader_module(wgpu::ShaderModuleDescriptor {
+                label: Some("CustomShaderPipeline shader"),
+                source: wgpu::ShaderSource::Wgsl(std::borrow::Cow::Borrowed(
+                    include_str!("shader.wgsl"),
+                )),
+            });
+
+        let pipeline =
+            device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+                label: Some("CustomShaderPipeline"),
+                layout: None,
+                vertex: wgpu::VertexState {
+                    module: &shader,
+                    entry_point: "vs_main",
+                    buffers: &[],
+                },
+                primitive: wgpu::PrimitiveState::default(),
+                depth_stencil: None,
+                multisample: wgpu::MultisampleState::default(),
+                fragment: Some(wgpu::FragmentState {
+                    module: &shader,
+                    entry_point: "fs_main",
+                    targets: &[Some(wgpu::ColorTargetState {
+                        format,
+                        blend: None,
+                        write_mask: wgpu::ColorWrites::ALL,
+                    })],
+                }),
+                multiview: None,
+            });
+        Self { pipeline }
+    }
+
+    fn update(&mut self) {}
+
+    fn render(
+        &self,
+        target: &wgpu::TextureView,
+        encoder: &mut wgpu::CommandEncoder,
+        viewport: Rectangle<u32>,
+    ) {
+        let mut pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+            label: Some("fill color test"),
+            color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+                view: target,
+                resolve_target: None,
+                ops: wgpu::Operations {
+                    //load: wgpu::LoadOp::Load,
+                    load: wgpu::LoadOp::Clear(Color {
+                        r: 1.0,
+                        g: 0.0,
+                        b: 1.0,
+                        a: 0.0,
+                    }),
+                    store: wgpu::StoreOp::Store,
+                },
+            })],
+            depth_stencil_attachment: None,
+            timestamp_writes: None,
+            occlusion_query_set: None,
+        });
+
+        pass.set_pipeline(&self.pipeline);
+        pass.set_scissor_rect(
+            viewport.x,
+            viewport.y,
+            viewport.width,
+            viewport.height,
+        );
+
+        pass.draw(0..4, 0..1);
+    }
+}
+
+#[derive(Debug)]
+struct CustomShaderPrimitive {}
+
+impl CustomShaderPrimitive {
+    fn new() -> Self {
+        Self {}
+    }
+}
+
+impl shader::Primitive for CustomShaderPrimitive {
+    fn prepare(
+        &self,
+        format: wgpu::TextureFormat,
+        device: &wgpu::Device,
+        _queue: &wgpu::Queue,
+        _bounds: Rectangle,
+        _target_size: Size<u32>,
+        _scale_factor: f32,
+        storage: &mut shader::Storage,
+    ) {
+        if !storage.has::<CustomShaderPipeline>() {
+            storage.store(CustomShaderPipeline::new(device, format));
+        }
+
+        let pipeline = storage.get_mut::<CustomShaderPipeline>().unwrap();
+
+        //upload data to GPU
+        pipeline.update();
+    }
+
+    fn render(
+        &self,
+        storage: &shader::Storage,
+        target: &wgpu::TextureView,
+        _target_size: Size<u32>,
+        viewport: Rectangle<u32>,
+        encoder: &mut wgpu::CommandEncoder,
+    ) {
+        //at this point our pipeline should always be initialized
+        let pipeline = storage.get::<CustomShaderPipeline>().unwrap();
+
+        //render primitive
+        pipeline.render(target, encoder, viewport);
+
+        // cannot clear textureview :E encoder.clear_texture(
+    }
+}
+
+struct CustomShaderProgram {}
+
+#[derive(Debug, Clone)]
+enum Message {
+    Tick(Instant),
+}
+
+impl<Message> shader::Program<Message> for CustomShaderProgram {
+    type State = ();
+    type Primitive = CustomShaderPrimitive;
+
+    fn draw(
+        &self,
+        _state: &Self::State,
+        _cursor: mouse::Cursor,
+        _bounds: Rectangle,
+    ) -> Self::Primitive {
+        CustomShaderPrimitive::new()
+    }
+}
+
+struct BasicShader {}
+
+impl Default for BasicShader {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl BasicShader {
+    fn new() -> Self {
+        Self {}
+    }
+
+    fn view(&self) -> Element<'_, Message> {
+        let shader = shader(CustomShaderProgram {})
+            .width(Length::Fill)
+            .height(Length::Fill);
+
+        shader.into()
+    }
+
+    fn update(&mut self, _message: Message) {}
+
+    fn subscription(&self) -> Subscription<Message> {
+        window::frames().map(Message::Tick)
+    }
+}
---
+++ examples/custom_shader_basic/src/shader.wgsl
@@ -0,0 +1,32 @@
+struct VertexIn {
+	@builtin(vertex_index) vertex_index: u32,
+}
+
+struct VertexOut {
+	@builtin(position) position: vec4f,
+	@location(0) uv: vec2f,
+}
+
+@vertex
+fn vs_main(in: VertexIn) -> VertexOut {
+	let uv = vec2f(vec2u((in.vertex_index << 1) & 2, in.vertex_index & 2));
+	let position = vec4f(uv * 2. - 1., 0., 1.);
+	return VertexOut(position, uv);
+}
+
+@fragment
+fn fs_main(in: VertexOut) -> @location(0) vec4f {
+	var p = in.uv;
+	var i: i32 = 0;
+	for (; i < 100; i = i + 1) {
+		let d = p * p;
+		if (d.x + d.y > 4.) {
+			break;
+		}
+
+		let next = vec2f(d.x - d.y + in.uv.x, 2. * p.x * p.y + in.uv.y);
+		p = next;
+	}
+
+	return vec4f(vec3f(f32(i) / 100.), 0.);
+}
